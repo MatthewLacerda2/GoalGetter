2d3aa85ca6206d0ca01ea4dbd13e6c6898295516
Matheus Lacerda <matheus.l1996@gmail.com>
2025-08-04 13:08:48 -0300

    better creation prompt

--- backend/utils/prompts.py
+++ backend/utils/prompts.py
@@ -6,28 +6,24 @@ The user has reached out for guidance on how to achieve a goal.
 You are an experienced expert in the user's chosen field who will act as a mentor.
 
 
-- The user was prompted with: {prompt_hint}
+- The user was hinted with: {prompt_hint}
 - The user's stated goal is: {prompt}
 
-Your task is to generate a list of follow-up questions. The answers will be used to a learning roadmap.
 
-Your questions should aim to uncover the user's:
-- Expectations and perception of the challenges ahead.
-- What interests the user in the subject
-- Experience in the subject. What do they know and/or can do
-- Skill level in the subject
-- Motivation for the goal. Why do they want it
+Your task is to generate a list of follow-up questions.
+You MUST find out the user's experience/knowledge of the subject, and what motivates the user to learn it.
+The answers will be used to generate a tailored roadmap.
 
-Guidelines:
-- Be direct and succinct
-- Consider how people typically progress in this subject area
-- You MUST find the user's skill level in the subject
-- You MUST find what will keep the user motivated
-- Focus on the user's experience/knowledge of the most basic fundamentals of the subject
-- Focus on the experience rather than knowledge, when the subject allows
+Your questions must:
+- Clarify the user's experience in the subject. What do they know and/or can do.
+- Identify what motivates the user in the subject.
+- Find out their expectations and perception of the challenges ahead.
 
-
-Return the questions as a **list of strings**, in the same language as the user's message.
+Your questions must be:
+- Direct and succinct.
+- Distinct from one another.
+- Avoid redundant questions or restating the user's goal.
+- Match the user's language
 """
 
 
@@ -36,41 +32,41 @@ def get_roadmap_initiation_prompt(roadmap_initiation_request: RoadmapInitiationR
 
 
 roadmap_creation_prompt = """
-You are an experienced expert who will give advice on how to achieve a goal.
+You are an experienced expert guiding a user to achieve a specific goal.
 
-
-The user was prompted with: {prompt_hint}
-The user's stated goal is: {prompt}
-Here is a series of follow-up questions that the user answered:
+- The user was prompted with: {prompt_hint}
+- The user's stated goal is: {prompt}
+- The user has answered the following follow-up questions:
 {questions_answers}
 
 
-Your task is to generate a list of tasks to help the user achieve that specific goal.
-The app will periodically check on the user and update the tasks if needed.
+Your task is to generate a clear list of tasks that will have the user reach their goal efficiently.
 
-The task can be:
-- With a well-defined end, which the user will do until they finish it
-- Open-ended, which the user will do until they're satisfied with their result
+We just tell the user what to do and give it an explanation.
+Periodically, we will check on the user's progress and answer some questions.
+The list of tasks will be updated periodically based on the user's progress.
 
-Your steps must:
-- Focus on having the user learn to self-correct as soon as possible
-- Prefer practice over theory whenever possible. Only include theoretical study if necessary for a practice later on
-- Be clear, direct and concise
-- Start from the most basic fundamentals the subject is yet to master
-
-Only the first few tasks should be detailed and/or specific.
-The latter ones are more broad and vague. They will be detailed by the app once the user gets to their level.
-
-Do NOT ask the user to show off to others unless it is necessary for the goal.
+We focus on:
+- Improving the user's most basic knowledge/skills on the subject.
+- Keeping the user motivated.
 
 
-You will include a list of notes. Those are things to know and common mistakes to avoid.
-They should focus only on the few first steps.
-These notes should be short and concise. Not more than 200 characters each.
-Not less than 1 and not more than 8 notes.
+Task guidelines:
+- Prioritize practice over theory.
+- Guarantee the user self-corrects if needed.
+- Avoid theory study unless the user can't/shouldn't learn it from practice, or if it'll speed up the learning process.
+- Avoid tasks that involve showing off to others unless essential for the goal.
 
-Write in the same language as the user's answer.
+
+Also provide a list of notes, focusing on things to know and common mistakes related to the first few tasks.
+- Between 2 and 10 notes.
+- Each note should be between 10 and 200 characters.
+- At least one note should be about stuff the user will eventually encounter, or mistakes the user will probably make.
+- At least one milestone should be declared.
+
+Write the roadmap and notes in the same language as the user's responses.
 """
+
 
 def questions_answers_to_string(questions_answers: list[FollowUpQuestionsAndAnswers]) -> str:
     return "\n".join([f"- {question.question}\n    {question.answer}" for question in questions_answers])
