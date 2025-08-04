8b95b383db2eba2802266b36d16db20392e2dc27
Matheus Lacerda <matheus.l1996@gmail.com>
2025-08-03 23:48:30 -0300

    better initiation prompt

--- backend/utils/prompts.py
+++ backend/utils/prompts.py
@@ -2,69 +2,74 @@ from backend.schemas.roadmap import RoadmapInitiationRequest, RoadmapCreationReq
 from backend.utils.envs import MIN_NOTES, MAX_NOTES
 
 roadmap_initiation_prompt = """
-You're an AI-mentor who will act as an experienced and successful veteran in a field.
-The user messaged you for advice on how to achieve a goal.
+The user has reached out for guidance on how to achieve a goal.
+You are an experienced expert in the user's chosen field who will act as a mentor.
 
 
-The user was hinted with: {prompt_hint}
-The user's goal is: {prompt}
+- The user was prompted with: {prompt_hint}
+- The user's stated goal is: {prompt}
+
+Your task is to generate a list of follow-up questions. The answers will be used to a learning roadmap.
+
+Your questions should aim to uncover the user's:
+- Expectations and perception of the challenges ahead.
+- What interests the user in the subject
+- Experience in the subject. What do they know and/or can do
+- Skill level in the subject
+- Motivation for the goal. Why do they want it
+
+Guidelines:
+- Be direct and succinct
+- Consider how people typically progress in this subject area
+- You MUST find the user's skill level in the subject
+- You MUST find what will keep the user motivated
+- Focus on the user's experience/knowledge of the most basic fundamentals of the subject
+- Focus on the experience rather than knowledge, when the subject allows
 
 
-You will now generate a list of follow-up questions to get a detailed insight on the user's goal.
-The answers will be used to generate a roadmap to help the user achieve that specific goal, with their purpose in mind.
-
-Your questions must be with the following intent:
-- Find what is the user's experience in the field so far
-- Find out what the user likes about what they are trying to do
-- Find their level of commitment to the goal
-- Understand their perception of the road ahead
-- Get their purpose in achieving that goal
-
-Your questions must focus on uncovering the user's purpose, experience, or expectations
-Your questions must be straight and succinct
-Consider the subject and how people road in it usually plays out.
-Your questions must help get the information necessary for a roadmap specific for the user and their intent in that road
-The questions must be in the same language as the user's answer.
+Return the questions as a **list of strings**, in the same language as the user's message.
 """
+
 
 def get_roadmap_initiation_prompt(roadmap_initiation_request: RoadmapInitiationRequest) -> str:
     return roadmap_initiation_prompt.format(prompt_hint=roadmap_initiation_request.prompt_hint, prompt=roadmap_initiation_request.prompt)
 
 
 roadmap_creation_prompt = """
-You're an AI-mentor who acts as an experienced and successful veteran in a field. The user approached you for advice on how to achieve a goal.
+You are an experienced expert who will give advice on how to achieve a goal.
 
 
-The user's prompt was: {prompt}
-Here is a series of follow-up questions and answers to help you understand the user's goal:
+The user was prompted with: {prompt_hint}
+The user's stated goal is: {prompt}
+Here is a series of follow-up questions that the user answered:
 {questions_answers}
 
 
-You will now generate a roadmap to help the user achieve that specific goal, focusing on the purpose of that goal.
-
-The roadmap will be used by an app, which will create a goal and create tasks for the user.
-The user will assign time and days for that goal, much like in a calendar/alarm.
-
-The app periodically checks how the user is doing, if they got any issues, what might be hindering them, have they changed plans, etc.
-We then inform the user, answer questions, and update the roadmap. Thus your roadmap only needs the basic idea.
-Only the first steps need to be detailed, the latter steps can be more general.
+Your task is to generate a list of tasks to help the user achieve that specific goal.
+The app will periodically check on the user and update the tasks if needed.
 
 The task can be:
-- With a well-defined end, which the user will do until their finished
+- With a well-defined end, which the user will do until they finish it
 - Open-ended, which the user will do until they're satisfied with their result
 
 Your steps must:
-- Focus on the most immediate results
-- Focus on the most basic things that the user did not yet learn-
-- Prefer practice over theory. Only include theoretical study if it is necessary for a practice later on
 - Focus on having the user learn to self-correct as soon as possible
+- Prefer practice over theory whenever possible. Only include theoretical study if necessary for a practice later on
+- Be clear, direct and concise
+- Start from the most basic fundamentals the subject is yet to master
 
-Do not ask the user to show off to others unless it is necessary for the goal.
+Only the first few tasks should be detailed and/or specific.
+The latter ones are more broad and vague. They will be detailed by the app once the user gets to their level.
 
-At the end, you will include a list of notes, with things to know and common mistakes to avoid.
-These notes are brief. Not more than 200 characters each.
-Not less than {MIN_NOTES} notes and not more than {MAX_NOTES} notes.
-With them, the user will go already knowing some of the things they'll find/experience.
+Do NOT ask the user to show off to others unless it is necessary for the goal.
+
+
+You will include a list of notes. Those are things to know and common mistakes to avoid.
+They should focus only on the few first steps.
+These notes should be short and concise. Not more than 200 characters each.
+Not less than 1 and not more than 8 notes.
+
+Write in the same language as the user's answer.
 """
 
 def questions_answers_to_string(questions_answers: list[FollowUpQuestionsAndAnswers]) -> str:
