58dd9388a3d9b7f98dc81921a4497aea85442473
Matheus Lacerda <matheus.l1996@gmail.com>
2025-08-11 21:53:25 -0300

    deleting not-needed translations

--- frontend/lib/l10n/app_localizations_en.dart
+++ frontend/lib/l10n/app_localizations_en.dart
@@ -12,13 +12,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get language => 'Language';
 
   @override
-  String get options => 'Options';
-
-  @override
   String get goals => 'Goals';
-
-  @override
-  String get agenda => 'Agenda';
 
   @override
   String get profile => 'Profile';
@@ -27,201 +21,16 @@ class AppLocalizationsEn extends AppLocalizations {
   String get howToUse => 'How to use';
 
   @override
-  String get about => 'About';
-
-  @override
-  String get name => 'Name';
-
-  @override
-  String get time => 'Time';
-
-  @override
-  String get setUpYourGoals => 'Set up your Goals';
-
-  @override
-  String get createFirstGoalMessage => 'Create your first goal to get started!';
-
-  @override
-  String goalDeleted(String goalTitle) {
-    return 'Goal \"$goalTitle\" deleted';
-  }
-
-  @override
-  String taskDeleted(String taskTitle) {
-    return 'Task \"$taskTitle\" deleted';
-  }
-
-  @override
-  String get undo => 'Undo';
-
-  @override
-  String get createNewGoal => 'Create New Goal';
-
-  @override
-  String get goalTitle => 'Goal Title';
-
-  @override
-  String get goalTitleHint => 'e.g., Read a book';
-
-  @override
-  String get pleaseEnterGoalTitle => 'Please enter a goal title';
-
-  @override
   String get description => 'Description (Optional)';
-
-  @override
-  String get goalOptional => 'Goal (optional)';
-
-  @override
-  String get descriptionHint => 'Describe your goal in detail...';
-
-  @override
-  String get weeklyTimeCommitment => 'Weekly Time Commitment';
-
-  @override
-  String get pleaseEnterWeeklyTime => 'Please enter weekly time commitment';
-
-  @override
-  String get weeklyTimeMustBeGreater => 'Weekly time must be greater than 0';
-
-  @override
-  String weeklyTimeCannotExceed(int maxHours) {
-    return 'Weekly time cannot exceed $maxHours hours';
-  }
-
-  @override
-  String get youCanCreateTasksLater => 'You can create tasks for this goal later';
 
   @override
   String get create => 'Create';
 
   @override
-  String get goalCreatedSuccessfully => 'Goal created successfully!';
-
-  @override
-  String get thisFieldIsRequired => 'This field is required';
-
-  @override
-  String get pleaseEnterValidNumber => 'Please enter a valid number';
-
-  @override
-  String get valueMustBeNonNegative => 'Value must be non-negative';
-
-  @override
-  String get hours => 'Hours';
-
-  @override
-  String get minutes => 'Minutes';
-
-  @override
-  String get minutesMustBeLessThan60 => 'Minutes must be less than 60';
-
-  @override
-  String get save => 'Save';
-
-  @override
-  String get goalUpdatedSuccessfully => 'Goal updated successfully!';
-
-  @override
-  String get goalUpdated => 'Goal updated';
-
-  @override
-  String get timeReserved => 'Time reserved';
-
-  @override
-  String get deleteGoal => 'Delete Goal';
-
-  @override
-  String areYouSureYouWantToDeleteGoal(String goalTitle) {
-    return 'Are you sure you want to delete the goal \"$goalTitle\"? This action cannot be undone.';
-  }
-
-  @override
-  String get cancel => 'Cancel';
-
-  @override
   String get delete => 'Delete';
 
   @override
-  String get commited => 'Commited';
-
-  @override
-  String get reserved => 'Reserved';
-
-  @override
-  String goalNeedsTime(String goalTitle, int minutesMissing) {
-    return '$goalTitle needs +${minutesMissing}min/w';
-  }
-
-  @override
-  String get dismiss => 'Dismiss';
-
-  @override
-  String get pleaseFillAllRequiredFields => 'Please fill all required fields.';
-
-  @override
   String get title => 'Title';
-
-  @override
-  String get createTask => 'Create Task';
-
-  @override
-  String get ifTheTaskHasASpecificGoal => 'If the task has a specific goal, you can select it here.';
-
-  @override
-  String get hour => 'Hour';
-
-  @override
-  String get pickTime => 'Pick Time';
-
-  @override
-  String get duration => 'Duration';
-
-  @override
-  String get editTask => 'Edit Task';
-
-  @override
-  String get sunday => 'SUN';
-
-  @override
-  String get monday => 'MON';
-
-  @override
-  String get tuesday => 'TUE';
-
-  @override
-  String get wednesday => 'WED';
-
-  @override
-  String get thursday => 'THU';
-
-  @override
-  String get friday => 'FRI';
-
-  @override
-  String get saturday => 'SAT';
-
-  @override
-  String get noActivitiesForTheDayYet => 'No activities for the day (yet)';
-
-  @override
-  String get noGoal => 'No goal';
-
-  @override
-  String get daysOfTheWeek => 'Days of the week';
-
-  @override
-  String goal(String goalTitle) {
-    return 'Goal: $goalTitle';
-  }
-
-  @override
-  String get deleteTask => 'Delete Task';
-
-  @override
-  String areYouSureYouWantToDeleteTask(String taskTitle) {
-    return 'Are you sure you want to delete the task \"$taskTitle\"? This action cannot be undone.';
-  }
 
   @override
   String get createYourGoal => 'Create your goal';
@@ -257,9 +66,6 @@ class AppLocalizationsEn extends AppLocalizations {
   String get skip => 'Skip';
 
   @override
-  String get previous => 'Previous';
-
-  @override
   String get next => 'Next';
 
   @override
@@ -290,7 +96,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get roadmap => 'Roadmap';
 
   @override
-  String get beforeYouStart => 'Just so you know...';
+  String get beforeYouStart => 'Before you start...';
 
   @override
   String get goalStarted => 'Goal started!';
@@ -303,7 +109,4 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String get pleaseAnswerThisQuestion => 'Please answer this question';
-
-  @override
-  String get orYouCanCreateAFullPlan => 'You can create a full:';
 }
