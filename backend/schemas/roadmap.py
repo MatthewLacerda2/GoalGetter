6be19b4c3b9c6ebaea23059e21a932e71cb53c90
Matheus Lacerda <matheus.l1996@gmail.com>
2025-08-20 02:18:31 -0300

    removed roadmap creation

--- backend/schemas/roadmap.py
+++ backend/schemas/roadmap.py
@@ -8,19 +8,3 @@ class RoadmapInitiationRequest(BaseModel):
 class RoadmapInitiationResponse(BaseModel):
     original_prompt: str = Field(..., description="The user's declaration of their goal")
     questions: list[str] = Field(..., description="The questions to ask the user to understand their goal")
-
-class FollowUpQuestionsAndAnswers(BaseModel):
-    question: str = Field(..., description="A question to understand the user's goal")
-    answer: str = Field(..., description="The user's answer to the question")
-
-class RoadmapCreationRequest(BaseModel):
-    prompt: str = Field(..., description="The user's declaration of their goal")
-    questions_answers: list[FollowUpQuestionsAndAnswers] = Field(..., description="The questions and answers to understand the user's goal")
-
-class Step(BaseModel):
-    title: str = Field(..., description="A title for the step")
-    description: str = Field(..., description="A description of the step")
-
-class RoadmapCreationResponse(BaseModel):
-    steps: list[Step] = Field(..., description="The steps to achieve the goal")
-    notes: list[str] = Field(..., description="The notes to help the user achieve the goal")
--- backend/utils/gemini.py
+++ backend/utils/gemini.py
@@ -3,8 +3,8 @@ from google.genai import Client
 from google.genai.types import GenerateContentConfig
 import logging
 from backend.core.config import settings
-from backend.schemas.roadmap import RoadmapInitiationResponse, RoadmapInitiationRequest, RoadmapCreationResponse, RoadmapCreationRequest
-from backend.utils.prompts import get_roadmap_initiation_prompt, get_roadmap_creation_prompt
+from backend.schemas.roadmap import RoadmapInitiationResponse, RoadmapInitiationRequest
+from backend.utils.prompts import get_roadmap_initiation_prompt
 
 logger = logging.getLogger(__name__)
 load_dotenv()
@@ -43,18 +43,3 @@ def get_gemini_follow_up_questions(initiation_request: RoadmapInitiationRequest)
     json_response = response.text
     
     return RoadmapInitiationResponse.model_validate_json(json_response)
-
-def get_gemini_roadmap_creation(creation_request: RoadmapCreationRequest) -> RoadmapCreationResponse:
-    client = get_client()
-    full_prompt = get_roadmap_creation_prompt(creation_request)
-    
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=full_prompt, config=GenerateContentConfig(
-        response_mime_type='application/json',
-        response_schema=RoadmapCreationResponse.model_json_schema(),
-        automatic_function_calling={"disable": True}
-    ),)
-    
-    json_response = response.text
-    
-    return RoadmapCreationResponse.model_validate_json(json_response)
