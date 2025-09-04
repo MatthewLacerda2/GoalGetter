1149b9fe3732c2a8301e66decc15923dfb60ed1f
Matheus Lacerda <matheus.l1996@gmail.com>
2025-09-04 18:27:46 -0300

    some structure sketches

---
+++ frontend/lib/screens/onboarding/report_screen.dart
@@ -0,0 +1,5 @@
+//TODO: report screen
+
+// it's like the 'tutor' is reporting on the student and his progress
+
+//look backend/models/student_context.py
---
+++ frontend/lib/screens/onboarding/signup_formulary_screen.dart
@@ -0,0 +1,2 @@
+//TODO: these are default questions for any user
+//we let the user answer them while we generate the follow-up questions
