24ee9105255f9cbda12f1bd5ddaf086ebd308dc4
Matheus Lacerda <matheus.l1996@gmail.com>
2025-09-17 20:44:39 -0300

    search websites

--- backend/utils/gemini/resources/ebooks/prompts.py
+++ backend/utils/gemini/resources/ebooks/prompts.py
@@ -1,11 +1,11 @@
-def get_search_ebooks_prompt_plain_text(goal_name: str, goal_description: str, objective_name: str, objective_description: str, student_context: list[str] | None):
+def get_search_ebooks_prompt_plain_text(goal_name: str, goal_description: str, objective_name: str, objective_description: str, student_context: list[str] | None) -> str:
     
     context_list: str = "Here is some context about the student:" + "\n".join([f"- {context}." for context in student_context]) if student_context else ""
     
     return f"""
     ## Task
     
-    You are a helpful assistant that searches for ebooks online.
+    You are a helpful assistant that searches ebooks online for the user.
         
     The user has the current objective '{objective_name}', with the description '{objective_description}'.
     The goal is '{goal_name}', with the description {goal_description}.
@@ -15,10 +15,10 @@ def get_search_ebooks_prompt_plain_text(goal_name: str, goal_description: str, o
     ## Instructions
     
     You have the Google Search tool at your disposal. Use it to search for ebooks available online.
+    Inserting 'filetype:pdf' at the end of the search queries helps yield better results.
     Only output links directly copied from the Google Search tool results
     
-    Inserting 'filetype:pdf' at the end of the search queries helps yield better results.
-    Just look at the title, description, and a bit of the content. If it sounds good and reads well, that's gold.
+    Just look at the title, description, and a bit of the content if available. If it sounds good and reads well, that's gold.
         
     The links MUST end in '.pdf'.
     The ebooks MUST be in the same language as the goal name and/or objective name.
@@ -38,7 +38,7 @@ def get_search_ebooks_prompt_plain_text(goal_name: str, goal_description: str, o
     Return no more than 10 results. Prioritize the results ranked first.
     """
 
-def get_search_ebooks_prompt(gemini_results_plain_text: str):
+def get_search_ebooks_prompt(gemini_results_plain_text: str) -> str:
     
     return f"""    
     You are a helpful assistant that formats text into a JSON object.
@@ -55,7 +55,7 @@ def get_search_ebooks_prompt(gemini_results_plain_text: str):
     - link: The link to the ebook
     
     You will return a list of ResourceSearchResultItem.
-    
+    You can just copy the search results as is.
     
     Here is the search results:
     {gemini_results_plain_text}
--- backend/utils/gemini/resources/search_resources.py
+++ backend/utils/gemini/resources/search_resources.py
@@ -1,15 +1,17 @@
 from typing import List
 from backend.utils.gemini.resources.schema import ResourceSearchResults, ResourceSearchResultItem
 from backend.utils.gemini.resources.ebooks.search import search_ebooks
+from backend.utils.gemini.resources.websites.search import search_websites
+from backend.utils.gemini.resources.youtubers.search import search_youtubers
 
 def search_resources(
     goal_name: str, goal_description: str, objective_name: str, objective_description: str, student_context: list[str] | None
 ) -> ResourceSearchResults:
     
     ebooks : ResourceSearchResults = search_ebooks(goal_name, goal_description, objective_name, objective_description, student_context)
-    youtubers : ResourceSearchResults = search_youtubers(goal_name, goal_description, objective_name, objective_description, student_context)
     websites : ResourceSearchResults = search_websites(goal_name, goal_description, objective_name, objective_description, student_context)
-    
+    youtubers : ResourceSearchResults = search_youtubers(goal_name, goal_description, objective_name, objective_description, student_context)
+    #TODO: add the resourcetype
     all_resources : List[ResourceSearchResultItem] = ebooks.resources + youtubers.resources + websites.resources
     
     return ResourceSearchResults(resources=all_resources)
