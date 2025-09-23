7ad3eb831f7a1d3498c452da351e33c47dca0c20
Matheus Lacerda <matheus.l1996@gmail.com>
2025-09-23 11:55:01 -0300

    enxugando onboarding, removing deleted routes

--- backend/api/v1/endpoints/__init__.py
+++ backend/api/v1/endpoints/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from backend.api.v1.endpoints import objective, onboarding, achievements, auth, chat, resources, streak, activities, assessments, learn_info, student
 from backend.core.rate_limiter import limiter
+from backend.api.v1.endpoints import objective, onboarding, achievements, auth, chat, resources, streak, student
 
 router = APIRouter()
 
@@ -15,7 +15,5 @@ router.include_router(chat.router, prefix="/chat", tags=["chat"])
 router.include_router(resources.router, prefix="/resources", tags=["resources"])
 router.include_router(streak.router, prefix="/streak", tags=["streak"])
 router.include_router(objective.router, prefix="/objective", tags=["objective"])
-router.include_router(activities.router, prefix="/activities", tags=["activities"])
-router.include_router(assessments.router, prefix="/assessments", tags=["assessments"])
 router.include_router(student.router, prefix="/student", tags=["student"])
 #router.include_router(learn_info.router, prefix="/learn_info", tags=["learn_info"])
