5ddd1ba5e8fe95531726566e88a1460c40e6fd2e
MatthewLacerda2 <lendacerda@gmail.com>
2026-06-06 00:22:29 -0300

    config: update default backend base URL to goalsgetter.org

--- frontend/lib/config/app_config.dart
+++ frontend/lib/config/app_config.dart
@@ -2,8 +2,7 @@ class AppConfig {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
     // No trailing slash here is usually safer if your SDK adds the leading slash
-    //defaultValue: 'https://goalsgetter.org',
-    defaultValue: 'http://192.168.0.9:8000',
+    defaultValue: 'https://goalsgetter.org',
   );
 
   static const String googleClientId = String.fromEnvironment(
